057ff53edbf2b20e8e0dddc8a922bf7cb33e9dda
Daniel Stahl <danstahl1138@gmail.com>
2021-07-19 15:43:37 -0500

    mild improvements to logging

--- test_container/run.sh
+++ test_container/run.sh
@@ -1,1 +1,1 @@
-/opt/spark/bin/spark-submit --master local[*] /home/sparkpy/main.py "devapp" "{\"max_file_age\": \"2d\"}" "$(cat example.json)"
+/opt/spark/bin/spark-submit --master local[*] --conf "spark.driver.extraJavaOptions=-Dlog4j.configuration=file:/home/sparkpy/log4j.properties" --conf "spark.executor.extraJavaOptions=-Dlog4j.configuration=file:/home/sparkpy/log4j.properties" /home/sparkpy/main.py "devapp" "{\"max_file_age\": \"2d\"}" "$(cat example.json)"
